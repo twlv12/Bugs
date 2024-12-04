0276c519a954d0f6566c0ca948f7cf8739e10746
E.T. <ethantwelves17@gmail.com>
2024-12-04 21:09:59 +0000

    -Finished createGenome() -Made visualizer.py

--- main.py
+++ main.py
@@ -1,38 +1,87 @@
 import random as r
 import pygame as pg
+import numpy as np
+import visualizer
 
-inputNeurones = {
-    "random" : 0000,
-    "age" : 0001,
-    "blockagex" : 0010,
-    "blockagey" : 0011,
-    "oscillator" : 0100,
-    "positionx" : 0101,
-    "positiony" : 0110,
-    "popdensity" : 0111,
-    "borderx" : 1000,
-    "bordery" : 1001,
-    "lastmovex" : 1010,
-    "lastmovey" : 1010,
-    "constant" : 1011,
+inputNeurons = {
+    "random" : "00001",
+    "age" : "00010",
+    "blockageleft" : "00011", 
+    "blockageright" : "00100",
+    "oscillator" : "00101",
+    "positionx" : "00110",
+    "positiony" : "00111",
+    "popdensity" : "01000",
+    "borderx" : "01001",
+    "bordery" : "01010",
+    "lastmovex" : "01011",
+    "lastmovey" : "01100",
+    "constant" : "01101",
+    "blockageup" : "01110",
+    "blockagedown" : "01111",
     }
 
-outputNeurones = {
-    "moveup" : 0000,
-    "movedown" : 0001,
-    "moveleft" : 0010,
-    "moveright" : 0011,
-    "moverandom" : 0100,
-    "setoscillator" : 0101,
+outputNeurons = {
+    "moveup" : "00001",
+    "movedown" : "00010",
+    "moveleft" : "00011",
+    "moveright" : "00100",
+    "moverandom" : "00101",
     }
+#00 -> input
+#01 -> output
+#10 -> internal
 
 worldSize = 128
 screenSize = 720
-genomeLength = 8
+genomeComplexity = 4
+
+#---TO-DO---
+#Implement connection conversion from binary and float remapping
+#Create brain structure using genome
+#Possibly filter redundant genes?
+# ¦> May slow evolution as creatures might not be able to make new neurons? ->ask chatgpt
+# ¦> start thinking about mutation function - bitwise mutation or random?
+
 
 bugsList = []
 
-def createBrain():
+def createGenome(genomeLength):
+    numInternalNeurons = 0
+    genome = ""
+    
+    for i in range(genomeLength):
+        #genes are 20 bits FORMAT:: 00(sType)  00000(sID)  00(tType)  00000(tID)  00(cType)  0000(Weight)  BITS:: 2 5 2 5 2 4
+
+        #creating random source and target neurons from dicts
+        sourceType = r.choice(["10", "00", "00"])
+        if sourceType == "00":
+            sourceID = r.choice(list(inputNeurons.values()))
+        elif sourceType == "10":
+            #internal neurons ids are created:: get next id -> bin -> string -> slice 0b prefix -> add leading zeros
+            sourceID = str(bin(numInternalNeurons)[2:].zfill(5))
+            numInternalNeurons += 1
+        sourceNeuron = sourceType + sourceID
+    
+        targetType = r.choice(["10", "01", "01"])
+        if targetType == "01":
+            targetID = r.choice(list(outputNeurons.values()))
+        elif targetType == "10":
+            targetID = str(bin(numInternalNeurons)[2:].zfill(5))
+            numInternalNeurons += 1
+        targetNeuron = targetType + targetID
+
+        #excitor/inhibitor/inverter
+        connectionType = r.choice(["00","01","10"]) #00-excitor 01-inhibitor 10-inverter
+        #creating random weight from 0-15 inclusive (will be remapped 0-1 when connection created)
+        weight = str(bin(r.randint(0,15))[2:].zfill(4))
+
+        #print(sourceType, sourceID, targetType, targetID, connectionType, weight)
+        gene = sourceNeuron + targetNeuron + connectionType + weight
+        genome += gene
+    return genome
+        
+def createBrain(genome):
     brain = Brain()
 
 def initializeBug(pos):
@@ -47,17 +96,17 @@ class Bug:
 class Brain:
     def __init__(self, genome):
         self.genome = genome
-        self.neurones = []
+        self.neurons = []
         self.connections = []
         
     def brainStep(self):
-        for neurone in self.neurones:
-            neurone.value = 0
+        for neuron in self.neurons:
+            neuron.value = 0
         for connection in self.connections:
             connection.transmit()
     
     
-class Neurone:
+class Neuron:
     def __init__(self, type, function, ID):
         self.type = type
         self.function = function
@@ -66,17 +115,20 @@ class Neurone:
             
         
 class Connection:
-    def __init__(self, source, target, type):
+    def __init__(self, source, target, type, weight):
         self.source = source
         self.target = target
         self.type = type
+        self.weight = weight
+        #MUST CONVERT THESE VALUES FROM BIN6
         
     def transmit(self):
-        if self.type == "excitor":
-            self.target.value += self.source.value
-        elif self.type == "inhibitor":
-            self.target.value -= self.source.value
+        if self.type == "excitor": #00
+            self.target.value += (self.source.value * self.weight)
+        if self.type == "inhibitor": #01
+            self.target.value -= (self.source.value * self.weight)
+        if self.type == "inverter" : #10
+            self.target.value = -(self.source.value * self.weight)
 
-pg.init()
-screen = pg.display.set_mode((screenSize, screenSize))
-clock = pg.time.Clock()
+
+visualizer.createGraph(createGenome(genomeComplexity))
---
+++ visualizer.py
@@ -0,0 +1,115 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+
+inputNeurons = {
+    "Random Value" : "00001",
+    "Age" : "00010",
+    "Blockage Left" : "00011", 
+    "Blockage Right" : "00100",
+    "Oscillator" : "00101",
+    "X Position" : "00110",
+    "Y Position" : "00111",
+    "Bug Density" : "01000",
+    "X Border Dist" : "01001",
+    "Y Border Dist" : "01010",
+    "Last X Move" : "01011",
+    "Last Y Move" : "01100",
+    "Constant" : "01101",
+    "Blockage Above" : "01110",
+    "Blockage Below" : "01111",
+    }
+
+outputNeurons = {
+    "Move Up" : "00001",
+    "Move Down" : "00010",
+    "Move Left" : "00011",
+    "Move Right" : "00100",
+    "Jitter" : "00101",
+    }
+
+def decode(genome):
+    decodedGenome = []
+    for i in range(len(genome)//20):
+        gene = genome[:20]
+
+        if gene[:2] == "00": 
+            sourceDict = inputNeurons.copy()
+            sourceType = "input"
+        if gene[:2] == "01": 
+            sourceDict = outputNeurons.copy()
+            sourceType = "output"
+        if gene[:2] == "10": 
+            sourceDict = "internal"
+            sourceType = "internal"
+
+        if sourceDict != "internal":
+            #get neuron function from correct table using source indicator
+            sourceID = list(sourceDict.keys())[list(sourceDict.values()).index(str(gene[2:7]))]
+        else: sourceID = "Internal"+str(int(gene[2:7], 2))
+
+        if gene[7:9] == "00": 
+            targetDict = inputNeurons.copy()
+            targetType = "input"
+        if gene[7:9] == "01": 
+            targetDict = outputNeurons.copy()
+            targetType = "output"
+        if gene[7:9] == "10": 
+            targetDict = "internal"
+            targetType = "internal"
+
+        if targetDict != "internal":
+            targetID = list(targetDict.keys())[list(targetDict.values()).index(str(gene[9:14]))]
+        else: targetID = "Internal"+str(int(gene[9:14], 2))
+
+        if gene[14:16] == "00": connectionType = "excitor"
+        elif gene[14:16] == "01": connectionType = "inhibitor"
+        elif gene[14:16] == "10": connectionType = "inverter"
+        
+        #converting binary weight to int 0-15 then remapped to 0-1
+        rawWeight = int(gene[16:], 2)
+        mappedWeight = round(np.interp(rawWeight, [0,15], [0.5,1]), 2)
+
+        gene = [sourceType, sourceID, targetType, targetID, connectionType, mappedWeight]
+        decodedGenome.append(gene)
+        genome = genome[20:]
+    return decodedGenome
+
+
+def createGraph(input):
+    genome = decode(input)
+
+    graph = nx.DiGraph()
+    for gene in genome:
+        if gene[1] not in graph:
+            graph.add_node(gene[1], type=gene[0])
+        if gene[3] not in graph:
+            graph.add_node(gene[3], type=gene[2])
+        
+        graph.add_edge(gene[1], gene[3], connectionType=gene[4], weight=gene[5])
+
+    nodeColours = []
+    for node in graph.nodes():
+        if graph.nodes[node]["type"] == "input":
+            nodeColours.append("lightgreen")
+        if graph.nodes[node]["type"] == "output":
+            nodeColours.append("red")
+        if graph.nodes[node]["type"] == "internal":
+            nodeColours.append("grey")
+
+    connectionColours = []
+    connectionWidths = []
+    for source, target, data in graph.edges(data=True):
+        if data["connectionType"] == "excitor":
+            connectionColours.append("lightgreen")
+        elif data["connectionType"] == "inhibitor":
+            connectionColours.append("red")
+        elif data["connectionType"] == "inverter":
+            connectionColours.append("blue")
+
+        connectionWidth = data["weight"] * 5
+        connectionWidths.append(connectionWidth)
+
+    layout = nx.circular_layout(graph)
+    nx.draw(graph, layout, with_labels=True, node_size=4000, font_weight="bold", node_color=nodeColours, edge_color=connectionColours, width=connectionWidths, font_size=10)
+    plt.show()
